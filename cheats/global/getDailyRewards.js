323bfdfecd4b5974d9d0beb690161a36e8d8b4c2
bombaclatcheater <kmunt1112@gmail.com>
2024-06-29 09:56:22 +1000

    Update getDailyRewards.js

--- cheats/global/getDailyRewards.js
+++ cheats/global/getDailyRewards.js
@@ -48,7 +48,7 @@
                 credentials: "include"
             }).catch(() => alert('There was an error when spoofing stats.'));
             await fetch("https://play.blooket.com/api/users/add-rewards", {
-                body: JSON.stringify({ t, addedTokens: 500, addedXp: 300 }),
+                body: JSON.stringify({ t, addedTokens: 5000, addedXp: 300 }),
                 method: "PUT",
                 credentials: "include"
             }).then(x => x.json())
